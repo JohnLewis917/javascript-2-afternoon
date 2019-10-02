c4fa0849374b2d0c0fb269294c07f9c2e5c7a0d3
John Lewis <johnlewis@MacBook-Pro.local>
2019-10-02 14:56:04 -0600

    new changes

--- practice.js
+++ practice.js
@@ -64,13 +64,16 @@ favoriteThings.book = 'Harry Potter'
 */
 
 //Code here
-
+let backPack = {}
+let item = 'firstPocket'
+backPack[item] = "chapstick"
+backPack.color = 'blue'
 /*
   After you do the above, alert your entire backPack object.
 */
 
 //Code here
-
+console.log(backPack)
 /*
 You probably noticed that it just alerted [object Object].
 Alerting to see the data in your Object doesn't work so well.
@@ -99,7 +102,8 @@ var user2 = {
 */
 
 //Code Here
-
+user2.name = 'Bryan G. Smith'
+user2.email = 'bryan.smith@devmounta.in'
 /////////////////////// EXTRA PRACTICE PROBLEMS BELOW ////////////////////
 ////////// MOVE ONTO NEXT SECTION BEFORE WORKING ON THESE ////////////////
 
